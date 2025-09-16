23214ef62e67e105ee3f0f03d2403b8865fb65bc
onionmccabbage <onionmccabbage@live.com>
2025-09-16 11:52:29 +0100

    using a class  which inherits from Thread

--- using_threads/thread_inheritance.py
+++ using_threads/thread_inheritance.py
@@ -1,1 +1,30 @@
 from threading import Thread
+import time
+import random
+
+class MyClass(Thread): # we inherit from the Thread class
+    '''to use a clas inheriting from Threadwe must include a run method'''
+    def __init__(self, n):
+        Thread.__init__(self)
+        self.n = n
+    def run(self):
+        for i in range(0,6):
+            print(f'{self.n} is working...')
+            time.sleep(random.random()*0.1)
+
+def main():
+    '''invoke the class as a Thread'''
+    # we can work with a large quantity of new threads
+    thread_list = []
+    for _ in range(0,16):
+        thread_list.append( MyClass(_) )
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list:
+        thread.join()
+    print('back on the main thread')
+    
+
+
+if __name__ == '__main__':
+    main()
