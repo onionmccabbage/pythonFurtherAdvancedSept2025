709d6c808ef17058c314cd9e4cb139b24c8318df
onionmccabbage <onionmccabbage@live.com>
2025-09-16 11:58:29 +0100

    working with very many threads, and using timeit

--- using_threads/thread_inheritance.py
+++ using_threads/thread_inheritance.py
@@ -1,6 +1,8 @@
 from threading import Thread
 import time
 import random
+# use timeit for accurate timing
+import timeit
 
 class MyClass(Thread): # we inherit from the Thread class
     '''to use a clas inheriting from Threadwe must include a run method'''
@@ -16,15 +18,17 @@ def main():
     '''invoke the class as a Thread'''
     # we can work with a large quantity of new threads
     thread_list = []
-    for _ in range(0,16):
+    for _ in range(0,1024):
         thread_list.append( MyClass(_) )
+    start = timeit.default_timer()
     for thread in thread_list:
         thread.start()
     for thread in thread_list:
         thread.join()
-    print('back on the main thread')
+    end = timeit.default_timer()
+    # NB most of the time is spent with I/O bound operations such as 'print'
+    print(f'total execution time: {end-start}')
     
-
 
 if __name__ == '__main__':
     main()
