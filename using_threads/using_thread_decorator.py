ca1c6ed1961825a8880f1a3f47451be92e9e5485
onionmccabbage <onionmccabbage@live.com>
2025-09-17 11:26:47 +0100

    writing decorators to make class methods thread-safe

--- using_threads/using_thread_decorator.py
+++ using_threads/using_thread_decorator.py
@@ -18,7 +18,7 @@ def lock_a_method(meth):
             # result = meth(self, *args, **kwargs)
             # lk.release()
             with lk: # the lock gets applied here see https://www.pythontutorial.net/python-concurrency/python-threading-lock/
-                # lk.acquire()
+                # lk.acquire() # do not acquire() it is already acquired by 'with'
                 return meth(self, *args, **kwargs)
             # the lock will be automaticaly released when the 'with' operator is done
     # we need to assign a sensible name to our new method
@@ -26,16 +26,28 @@ def lock_a_method(meth):
     locked_method.__is_locked = True # place a flag to indicate this method can be locked
     return locked_method # this contains a lockable version of the original method, renamed acordingly
 
-def make_thread_safe():
+def make_thread_safe(cls, meth_list, lk): # we choose to pass in a class, a list of method of that class to be locked, and a lock instance
     '''iterate over methods of a class, applying thread-safe lock to each'''
+    init = cls.__init__ # make a copy of the initializer method of the class
+    def new_init(self, *args, **kwargs):
+        init(self, *args, **kwargs) # call teh original initializer method
+        self.__lock = lk
+    cls.__init__ = new_init # replace the original init with this new one
+    # we iterate over eahc method in the method list, applying our lock safe function
+    for meth in meth_list:
+        old_meth = getattr(cls, meth)
+        new_meth = lock_a_method(old_meth) # a lockable version of the old method
+        setattr(cls, meth, new_meth) # replace the original method with a lockable version
+    return cls # the original class, but with lockable methods
 
-def lock_a_class():
+def lock_a_class(meth_list, lk):
     '''use this as a decorator'''
+    return lambda cls: make_thread_safe(cls,meth_list, lk)
 
 class MySet(set):
     '''inherit from the 'set' object with custom methods'''
     def __init__(self, other_set):
-        set.__init__(self, other_set) # calll the initializer of the set class
+        set.__init__(self, other_set) # call the initializer of the set class
     @lock_a_method # apply the decorator to just one method
     def my_method(self, new_value):
         '''this method only allows int values to be added'''
