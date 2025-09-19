4289ccaa0a6ee9ef794bee2aef72b61e6392d13d
onionmccabbage <onionmccabbage@live.com>
2025-09-19 12:03:11 +0100

    working with bytes

--- microservices/my_server_async.py
+++ microservices/my_server_async.py
@@ -5,7 +5,12 @@ from file_writer import writeBytes
 async def handleRequests(reader, writer):
     '''this server will expect requests from clients and send simple responses'''
     data = await reader.read(1024) # only the first bytes of the request
-    message = data.decode() # convert the bytes to a string
+    message = 'default (can\'t convert from bytes)'
+    try:
+        message = data.decode() # try to convert the bytes to a string
+    except UnicodeDecodeError as err:
+        pass
+
     address = writer.get_extra_info('peername') # this will be procided by the write object
     print(f'Server received {message} from {address}')
     writeBytes(data) # write the original bytes
