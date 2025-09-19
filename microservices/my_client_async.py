79860f8851fb96ed927e040042678d423748c259
onionmccabbage <onionmccabbage@live.com>
2025-09-19 12:00:24 +0100

    using non-text bytes

--- microservices/my_client_async.py
+++ microservices/my_client_async.py
@@ -4,17 +4,24 @@ import sys
 
 async def my_client(message):
     '''an asynchronous client to send a message to the server'''
+    # convert the incoming to bytes
+    b = makeBytes(message)
     reader, writer = await asyncio.open_connection('localhost', 8888)
-    print(f'Sending message {message}')
+    print(f'Sending message {b}')
     # all communication is over http so we must encode everything
-    writer.write(message.encode())
+    writer.write(b)
     await writer.drain() # useful for large data streams
     writer.close()
     # reader.close()
+
+def makeBytes(values):
+    '''convert teh values to bytes'''
+    b = bytes(values)
+    return b
 
 if __name__ == '__main__':
     if len(sys.argv) >1:
         msg = ' '.join(sys.argv[1:])
     else:
-        msg = 'default'
+        msg = range(0,256)
     asyncio.run(my_client(msg))
