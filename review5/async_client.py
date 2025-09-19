f00b8b7a191bb2cbe0a5665ee6c0e221efaa4571
onionmccabbage <onionmccabbage@live.com>
2025-09-19 14:31:47 +0100

    sample solution for review 5

---
+++ review5/async_client.py
@@ -0,0 +1,38 @@
+import asyncio
+import sys
+
+async def my_client(message):
+    '''a simple asynchronous client
+    send a message to the async server'''
+    reader, writer = await asyncio.open_connection('localhost', 8889)
+    print(f'Sending {message}')
+    writer.write(message.encode()) # all communication over http needs to be encoded
+    await writer.drain() # asynchronously wait until the writer has completed
+    writer.close()
+
+# a function to create encoded bytes
+# The majority of computer files are not human readable - they are byte files
+def makeBytes(values):
+    # lets make some bytes
+    b = bytes( values ) 
+    print(b) # print will nicely format the bytes as text
+    return b
+
+
+def writeBytes(b):
+    # persist these bytes into a file
+    try:
+        fout = open('bfile', 'ab') # 'w' will (over)write, 'b' for bytes
+        fout.write(b)
+        fout.close()
+    except FileExistsError as err:
+        print(f'File already exists {err}')
+    except Exception as err:
+        print(f'something went wrong {err}')
+
+if __name__ == '__main__':
+    if len(sys.argv)>1:
+        m = sys.argv[1]
+    else:
+        m='species'
+    asyncio.run(my_client(m))
---
+++ review5/async_server.py
@@ -0,0 +1,43 @@
+import asyncio
+from swapi_service import SwapiService
+
+# we use async and await together
+async def handleRequests(reader, writer):
+    '''This server will expect to receive requests from clients
+    Responses will be simple'''
+    data = await reader.read(1024) # read in the first 1024 bytes from the incoming data stream
+    message = data.decode() # convert the bytes to a string
+    addr = writer.get_extra_info('peername')
+    print(f'Received {message} from {addr}')
+    # additional features
+    # call Swapi Service
+    # do we have a permitted number (as a string)?
+    if message in ('1','2','3','4','5','6','7'):
+        response = SwapiService.getSwapi('people', message)
+    else:
+        response = SwapiService.getSwapi(message, 1)
+    print(response)
+    # commit every message received to a byte file
+    writeBytes(data)
+
+def writeBytes(n):
+    '''persist n in a byte file'''
+    print(f'writing {n} to byte file')
+    # we need a file access object
+    with open('my_byte_log', 'ab') as fout: # 'a' will append 'b' for bytes (default is 't' for text)
+        if type(n) == bytes:
+            fout.write(n)
+        elif type(n) == str:
+            fout.write(n.encode())
+        fout.write(b'\n') # a new line character as a byte string
+
+async def main():
+    '''start the microservice server'''
+    server = await asyncio.start_server(handleRequests, 'localhost', 8889) # localhost is 127.0.0.1
+    print('Server started')
+    # the server needs to run endlessly
+    async with server:
+        await server.serve_forever()
+
+if __name__ == '__main__':
+    asyncio.run( main() )
