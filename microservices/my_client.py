335d74a3bd4432f56471d16f572ef48f18f7355a
onionmccabbage <onionmccabbage@live.com>
2025-09-19 10:13:02 +0100

    a very simple socket client

--- microservices/my_client.py
+++ microservices/my_client.py
@@ -3,4 +3,12 @@ import socket
 
 def client():
     '''simple socket client to consume microservices'''
-    
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    setup_t = ('localhost', 9876) 
+    client.connect(setup_t) # we connect to the server
+    # next we can send abyte stream to te server
+    client.send(b'Hello from client')
+
+
+if __name__ == '__main__':
+    client()
