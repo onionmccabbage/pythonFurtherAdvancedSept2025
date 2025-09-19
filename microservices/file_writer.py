4ddbe82095f94c50fb884cf8be47d73106741acc
onionmccabbage <onionmccabbage@live.com>
2025-09-19 11:28:19 +0100

    byte writing with new line character

--- microservices/file_writer.py
+++ microservices/file_writer.py
@@ -3,5 +3,7 @@ def writeBytes(n):
     with open('my_bytes', 'ab') as fout: # 'ab' will append to a byte file
         if type(n)==bytes:
             fout.write(n)
+            # we may choose to add a termination character
+            fout.write('\n'.encode())
         else:
             fout.write(n.encode())
